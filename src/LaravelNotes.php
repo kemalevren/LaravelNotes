005c4a0ae12237cd1be84f46598bdb591c3f778f
Kemal Evren <kemal.evren@vargonen.com>
2023-06-11 14:27:44 +0300

    fix.

--- src/LaravelNotes.php
+++ src/LaravelNotes.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Arcanedev\LaravelNotes;
+namespace kemalevren\LaravelNotes;
 
 /**
  * Class     LaravelNotes
--- src/LaravelNotesServiceProvider.php
+++ src/LaravelNotesServiceProvider.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Arcanedev\LaravelNotes;
+namespace kemalevren\LaravelNotes;
 
 use Arcanedev\Support\Providers\PackageServiceProvider;
 
